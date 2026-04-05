d233d1277beb2fd13f914eaae68e5d297a896238
flyvn <younista666@gmail.com>
2026-04-05 16:55:55 +0700

    Split Header into component and use Link

--- ecommerce/src/pages/OrdersPage.jsx
+++ ecommerce/src/pages/OrdersPage.jsx
@@ -1,37 +1,11 @@
-import "./header.css";
+import { Header } from "../components/Header";
+
 import "./OrdersPage.css";
 
 export function OrdersPage() {
   return (
     <>
-      <div className="header">
-        <div className="left-section">
-          <a href="" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
+      <Header />
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
