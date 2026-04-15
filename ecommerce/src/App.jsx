7fe5e8a884437350f806466bf124875a161e5c4c
flyvn <younista666@gmail.com>
2026-04-15 14:33:31 +0700

    Generate the HTML for the orders page

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrdersPage />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<Error404Page />} />
     </Routes>
